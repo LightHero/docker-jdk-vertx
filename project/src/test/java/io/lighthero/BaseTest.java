de491f4ae73e039be89a26cf4ad6b64355630ef9
ufoscout <ufoscout@gmail.com>
2018-05-20 08:32:19 +0200

    First commit

---
+++ project/src/test/java/io/lighthero/BaseTest.java
@@ -0,0 +1,14 @@
+package io.lighthero;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class BaseTest {
+
+    @Test
+    public void isTrue() {
+        assertTrue(true);
+    }
+
+}
